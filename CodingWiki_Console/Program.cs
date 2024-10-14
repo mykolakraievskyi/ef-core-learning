24abb590cdfb553eeb339acc638c511600475196
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-10-14 15:03:04 +0300

    updates

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -16,35 +16,35 @@ Console.WriteLine("Hello, World!");
 // }
 //AddBook();
 //GetAllBooks();
-GetBook();
-return;
-
-void GetBook()
-{
-    //EF.Functions.
-    using var context = new ApplicationDbContext();
-    var book = context.Books.FirstOrDefault(x => x.Publisher_Id == 3);
-    Console.WriteLine(book);
-}
-void GetAllBooks()
-{
-    using var context = new ApplicationDbContext();
-    var books = context.Books.ToList();
-    books.ForEach(x => Console.WriteLine(x.Title + " - " + x.ISBN));
-}
-
-void AddBook()
-{
-    var book = new Book()
-    {
-        Title = "New EF Core book",
-        ISBN = "978-1-84356-028-2",
-        Price = 10.93m,
-        Publisher_Id = 1
-    };
-    using var context = new ApplicationDbContext();
-    context.Books.Add(book);
-    context.SaveChanges();
-
-}
+// GetBook();
+// return;
+//
+// void GetBook()
+// {
+//     //EF.Functions.
+//     using var context = new ApplicationDbContext();
+//     var book = context.Books.FirstOrDefault(x => x.Publisher_Id == 3);
+//     Console.WriteLine(book);
+// }
+// void GetAllBooks()
+// {
+//     using var context = new ApplicationDbContext();
+//     var books = context.Books.ToList();
+//     books.ForEach(x => Console.WriteLine(x.Title + " - " + x.ISBN));
+// }
+//
+// void AddBook()
+// {
+//     var book = new Book()
+//     {
+//         Title = "New EF Core book",
+//         ISBN = "978-1-84356-028-2",
+//         Price = 10.93m,
+//         Publisher_Id = 1
+//     };
+//     using var context = new ApplicationDbContext();
+//     context.Books.Add(book);
+//     context.SaveChanges();
+//
+// }
 
