73062e34b33d0292a5aaa66dc88723eedca67964
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-10-14 15:03:24 +0300

    has data is bette though

---
+++ CodingWiki_DataAccess/Migrations/20241014115911_seedingCategoriesThroughSQL.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingCategoriesThroughSQL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into categories values ('Cat 1')");
+            migrationBuilder.Sql("insert into categories values ('Cat 2')");
+            migrationBuilder.Sql("insert into categories values ('Cat 3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
